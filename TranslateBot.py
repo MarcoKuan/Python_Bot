892b55b02a24d4b81b08edc161ad28481a3ad92d
Marco Kuan <kuan.n.marco@gmail.com>
2019-06-05 04:46:20 -0700

    Hello @tags & Translator functionality (X -> EN) + EN -> CH also

--- TranslateBot.py
+++ TranslateBot.py
@@ -1,4 +1,8 @@
 import discord
+import googletrans
+
+# Create a translator object
+translator = googletrans.Translator()
 
 # The discord client
 client = discord.Client()
@@ -10,14 +14,31 @@ client = discord.Client()
 async def on_ready():
     print("The bot is ready!")
 
+
 # Responds to commands
 @client.event
 async def on_message(message):
+    # Checks that the bot is not the one who sent a message
     if message.author == client.user:
         return
 
+    # Hello test command
     if message.content.startswith("!hello"):
-        await message.channel.send('Hello!')
+        await message.channel.send("Hello! {}".format(message.author.mention))
 
+    if message.content.lower().startswith("!c "):
+        # Grab the message after the command
+        msg = message.content[3:]
+        # Get the language of the file
+        message_destination = translator.detect(msg).lang.lower()
+
+        if message_destination == "en":
+            # Directly converts English to Chinese (Traditional)
+            translated_message = translator.translate(msg, dest="zh-tw")
+            await message.channel.send("`{}` -> `{}`".format(msg, translated_message.text))
+        else:
+            # Translates the language and converts it to English
+            translated_message = translator.translate(msg)
+            await message.channel.send("`{}` -> `{}`".format(msg, translated_message.text))
 
 client.run(token)
